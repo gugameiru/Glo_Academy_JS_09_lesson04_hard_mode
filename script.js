178eae86908393ca0d3b2e5a3b914c595529bbb3
gugameiru <gugameiru@gmail.com>
2019-08-28 03:34:08 +0300

    Четвёртое усложненное ДЗ

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let myFunction = function(arg1) {
+    if (typeof arg1 === 'string' || arg1 instanceof String) {
+        let str = arg1.trim();
+        if (str.length > 30) {
+            return str.substring(0, 30) + '...';
+        }
+    } else {
+        return ('В функцию передана не строка');
+    } 
+};
+
+// Test passed
+console.log(myFunction('У попа была собака, он её любил, Она съела кусок мяса, он её убил, В землю закопал, Надпись написал'));
+
+// Test failed
+console.log(myFunction(555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555));
